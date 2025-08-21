c35600a2dc02a671a1db60c654a8fc6e3f6e4d53
javirerffggg <160790690+javirerffggg@users.noreply.github.com>
2025-08-21 14:20:38 +0000

    Create words.js

---
+++ words.js
@@ -0,0 +1,17 @@
+// Este archivo contiene todas las categorías y palabras para el juego Imposter Who?
+// Puedes añadir, editar o eliminar categorías y palabras fácilmente aquí.
+
+const WORD_CATEGORIES = {
+    "Animales": ["Perro", "Gato", "Elefante", "León", "Jirafa", "Tiburón", "Águila", "Serpiente", "Oso", "Lobo", "Tigre", "Cebra", "Mono", "Hipopótamo", "Cocodrilo", "Rinoceronte", "Panda", "Koala", "Canguro", "Pingüino"],
+    "Comida": ["Pizza", "Hamburguesa", "Sushi", "Paella", "Taco", "Ensalada", "Pasta", "Helado", "Sopa", "Chocolate", "Manzana", "Plátano", "Naranja", "Uvas", "Fresa", "Queso", "Pan", "Huevo", "Pollo", "Pescado"],
+    "Profesiones": ["Médico", "Profesor", "Bombero", "Policía", "Cocinero", "Ingeniero", "Artista", "Astronauta", "Abogado", "Periodista", "Carpintero", "Fontanero", "Electricista", "Mecánico", "Científico", "Músico", "Actor", "Deportista", "Piloto", "Veterinario"],
+    "Deportes": ["Fútbol", "Baloncesto", "Tenis", "Natación", "Béisbol", "Golf", "Boxeo", "Ciclismo", "Atletismo", "Rugby", "Voleibol", "Balonmano", "Hockey", "Esquí", "Surf", "Ajedrez", "Fórmula 1", "Gimnasia", "Judo", "Karate"],
+    "Películas Famosas": ["Titanic", "Star Wars", "El Padrino", "Jurassic Park", "Avatar", "Matrix", "El Señor de los Anillos", "Harry Potter", "Frozen", "Joker", "Forrest Gump", "Pulp Fiction", "El Rey León", "Regreso al Futuro", "Indiana Jones", "E.T.", "Toy Story", "Buscando a Nemo", "Shrek", "Gladiator"],
+    "Países": ["España", "México", "Japón", "Egipto", "Brasil", "Canadá", "Australia", "Italia", "China", "Rusia", "India", "Argentina", "Francia", "Alemania", "Reino Unido", "Estados Unidos", "Sudáfrica", "Corea del Sur", "Turquía", "Grecia"],
+    "Objetos de Casa": ["Silla", "Mesa", "Lámpara", "Sofá", "Cama", "Televisión", "Espejo", "Reloj", "Tenedor", "Ventana", "Puerta", "Armario", "Cuchillo", "Plato", "Vaso", "Ducha", "Toalla", "Ordenador", "Teléfono", "Libro"],
+    "Instrumentos Musicales": ["Guitarra", "Piano", "Violín", "Batería", "Trompeta", "Flauta", "Saxofón", "Arpa", "Bajo", "Acordeón", "Ukelele", "Clarinete", "Trombón", "Violonchelo", "Gaita", "Maracas", "Pandereta", "Xilófono", "Armónica", "DJ Mixer"],
+    "Superhéroes": ["Superman", "Batman", "Spiderman", "Iron Man", "Capitán América", "Thor", "Hulk", "Wonder Woman", "Aquaman", "Flash", "Black Panther", "Doctor Strange", "Ant-Man", "Lobezno", "Deadpool", "Catwoman", "Viuda Negra", "Ojo de Halcón", "Groot", "Rocket"],
+    "Videojuegos": ["Mario Bros", "Zelda", "Minecraft", "Fortnite", "Among Us", "Pac-Man", "Tetris", "Pokémon", "Sonic", "Grand Theft Auto", "League of Legends", "World of Warcraft", "The Sims", "FIFA", "Call of Duty", "Animal Crossing", "Street Fighter", "Mortal Kombat", "God of War", "Red Dead Redemption"],
+    "Marcas Famosas": ["Coca-Cola", "Nike", "Apple", "Google", "Amazon", "McDonald's", "Adidas", "Toyota", "Samsung", "Disney", "Lego", "Ikea", "Ferrari", "Microsoft", "Netflix", "Starbucks", "Ford", "Sony", "Pepsi", "H&M"],
+    "Cosas de la Naturaleza": ["Árbol", "Flor", "Río", "Montaña", "Playa", "Desierto", "Volcán", "Nube", "Sol", "Luna", "Estrella", "Arcoíris", "Océano", "Lago", "Bosque", "Cueva", "Cascada", "Roca", "Arena", "Hielo"]
+};
